0f05da02b4b4ed80ec94f455842984cb78ef02d5
LauraToddCode <laura20todd@gmail.com>
2021-03-30 18:07:34 +0100

    Starter code

---
+++ src/components/Contact.js
@@ -0,0 +1,7 @@
+function Contact() {
+    return (
+        <h1>I'm the contact page</h1>
+    )
+}
+
+export default Contact;
---
+++ src/components/Products.js
@@ -0,0 +1,7 @@
+function Products() {
+    return (
+        <h1>I'm the products page</h1>
+    )
+}
+
+export default Products;
