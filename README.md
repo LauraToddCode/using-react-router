a9d81864e096a01f17a8a1749601dd9db6e96a22
LauraToddCode <laura20todd@gmail.com>
2021-03-30 19:10:22 +0100

    added homepage to package.json

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
-# using-react-router
- Example code for a blog post
+# Using React Router
+ Example code for a [blog post](https://www.lauratodddesign.com/blog/a-really-simple-intro-to-react-router/) about using React Router.
+
+ I use a really simple example to create a navbar to navigate around a multi-page app.
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -3,15 +3,9 @@ import { Link } from "react-router-dom";
 function Navbar() {
     return (
         <ul>
-            <Link to="/">
-                <li>Home</li>
-            </Link>
-            <Link to="/products">
-                <li>Products</li>
-            </Link>
-            <Link to="/contact">
-                <li>Contact</li>
-            </Link>
+            <Link to="/">Home</Link>
+            <Link to="/products">Products</Link>
+            <Link to="/contact">Contact</Link>
         </ul>
     )
 }
