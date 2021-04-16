345521cbfa9d25ca2f5daf63c1922e17fb97d57d
LauraToddCode <laura20todd@gmail.com>
2021-04-16 14:51:43 +0100

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,16 +1,18 @@
 import Home from "./components/Home";
 import Products from "./components/Products";
 import Contact from "./components/Contact";
-import { Route } from "react-router-dom";
+import { Route, Switch } from "react-router-dom";
 import Navbar from "./components/Navbar";
 
 function App() {
     return (
         <div>
             <Navbar />
-            <Route exact path="/" component={ Home } />
-            <Route exact path="/products" component={ Products } />
-            <Route exact path="/contact" component={ Contact } />
+            <Switch>
+                <Route exact path="/" component={ Home } />
+                <Route exact path="/products" component={ Products } />
+                <Route exact path="/contact" component={ Contact } />
+            </Switch>
         </div>
     )
 }
